942437454149ae69a581de1fdc5902afea3c1aa1
otumian <popecan1000@gmail.com>
2019-12-12 12:03:36 +0000

    factorial implementation in lisp

---
+++ factorial.lisp
@@ -0,0 +1,9 @@
+;; factorial implementation in lisp
+;; do (compile-file "path/to/this/file")
+;; then on success do (load "path/to/this/file .fasl")
+;; now do (factorial (n)) ;; where is some integer value
+(defun factorial (n) 
+	(if (< n 2)
+		1
+		(* n (factorial (- n 1)))))
+		
